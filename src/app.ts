29fb0afe4dd98f82627f93ad46d64460bb565f0f
sayyed-ahsan <sayyed.ahsan.dev@gmail.com>
2023-11-23 21:40:50 +0600

    basic project setup done

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  const a = 10;
+  res.send(`hello${a}`);
+});
+
+export default app;
